731f003ae36af6415d0cdaf098673d63cb5d091d
Rohit Prajapati <153930477+rohheitt@users.noreply.github.com>
2026-08-01 20:09:53 +0530

    Time: 1 ms (99.95%), Space: 77.2 MB (66.81%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         int currSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+        int maxSum = nums[0];
         for(int i=0; i<nums.length; i++){
             currSum += nums[i];
             maxSum = Math.max(currSum, maxSum);
