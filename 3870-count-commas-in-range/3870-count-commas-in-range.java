4b8bfd068c81291b02e4afb65cc7cc0c252e2172
Rohit Prajapati <153930477+rohheitt@users.noreply.github.com>
2026-09-09 15:53:16 +0530

    Time: 4 ms (16.86%), Space: 42.3 MB (85.34%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n < 1000){
+            return 0;
+        }
+
+        int comma = 0;
+        for(int i=1000; i<=n; i++){
+            comma++;
+        }
+
+        return comma;
+    }
+}
