263725ee0659618eea2da36ee64348947dff6dd9
Rohit Prajapati <153930477+rohheitt@users.noreply.github.com>
2026-09-09 15:54:07 +0530

    Time: 1 ms (83.58%), Space: 42.8 MB (8.36%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -4,11 +4,6 @@ class Solution {
             return 0;
         }
 
-        int comma = 0;
-        for(int i=1000; i<=n; i++){
-            comma++;
-        }
-
-        return comma;
+        return n-999;
     }
 }
