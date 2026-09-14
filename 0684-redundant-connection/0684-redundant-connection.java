c2cda5b6146440172d8f6b60d3a753e3f15bef6b
Rohit Prajapati <153930477+rohheitt@users.noreply.github.com>
2026-09-14 14:40:43 +0530

    Time: 1 ms (91.35%), Space: 45.1 MB (38.92%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,42 +1,32 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-        ArrayList<Integer>[] graph = new ArrayList[edges.length + 1];
-        for(int i=0; i<graph.length; i++){
-            graph[i] = new ArrayList<>();
+        int[] parent = new int[edges.length + 1];
+
+        for(int i=0; i<parent.length; i++){
+            parent[i] = i;
         }
 
         for(int[] edge : edges){
             int u = edge[0];
             int v = edge[1];
 
-            boolean[] visited = new boolean[graph.length];
+            int rootU = find(u, parent);
+            int rootV = find(v, parent);
 
-            if(dfs(graph, u, v, visited)){
-                return new int[] {u, v};
+            if(rootU == rootV){
+                return new int[]{u, v};
             }
-
-            graph[edge[0]].add(edge[1]);
-            graph[edge[1]].add(edge[0]);
-
+            parent[rootV] = rootU;
         }
 
         return new int[] {};
     }
 
-    public boolean dfs(ArrayList<Integer>[] graph, int u, int v, boolean[] visited){
-        if(u == v){
-            return true;
-        }
-        visited[u] = true;
-
-        for(int neighbour : graph[u]){
-            if(!visited[neighbour]){
-                if(dfs(graph, neighbour, v, visited)){
-                    return true;
-                }
-            }
+    public int find(int x, int[] parent){
+        if(parent[x] == x){
+            return x;
         }
 
-        return false;
+        return find(parent[x], parent);
     }
 }
